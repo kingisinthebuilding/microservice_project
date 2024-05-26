78ccb4d32a11339eaf5e6710f9b5b385e647e58d
Siddhesh Gupta <siddheshgupta596@gmail.com>
2024-05-26 22:53:33 +0530

    Microservice_Project

--- UserService/UserService/src/main/java/com/microservice/userservice/controller/UserController.java
+++ UserService/UserService/src/main/java/com/microservice/userservice/controller/UserController.java
@@ -2,6 +2,9 @@ package com.microservice.userservice.controller;
 
 import com.microservice.userservice.model.User;
 import com.microservice.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +32,9 @@ public class UserController
     }
 
     @GetMapping("/{id}")
+    //@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    //@Retry(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
         User user = userService.getUserById(id);
         if (user != null) {
@@ -36,6 +42,20 @@ public class UserController
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(user);
         }
+    }
+
+    //Creating fallback method for circuitbreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex)
+    {
+        ex.printStackTrace();
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234")
+                .build();
+        System.out.println("User : " + user);
+        return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/{id}")
--- UserService/UserService/src/main/java/com/microservice/userservice/model/User.java
+++ UserService/UserService/src/main/java/com/microservice/userservice/model/User.java
@@ -13,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Builder
 public class User
 {
     @Id
